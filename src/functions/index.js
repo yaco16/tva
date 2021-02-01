9d8ee47e3c7e54d06801fd320b2d77f7a31e2451
yaco16 <65079827+yaco16@users.noreply.github.com>
2021-02-01 23:22:16 +0100

    refactoring create directory for functions update functions with arrow functions create directory for data

---
+++ src/functions/index.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-undef */
+import { startValues } from '../data';
+
+export const createTable = () => {
+  Excel.run(function(context) {
+    const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+    const expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
+    expensesTable.name = "ExpensesTable";
+
+    expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
+
+    expensesTable.rows.add(null /*add at the end*/, startValues);
+
+    expensesTable.columns.getItemAt(3).getRange().numberFormat = [["\u20AC#,##0.00"]];
+    expensesTable.getRange().format.autofitColumns();
+    expensesTable.getRange().format.autofitRows();
+
+    return context.sync();
+  }).catch(function(error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+export const sortTable = () => {
+  Excel.run(function(context) {
+    const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+    const expensesTable = currentWorksheet.tables.getItem("ExpensesTable");
+    const sortFields = [
+      {
+        key: 1, // Merchant column
+        ascending: false
+      }
+    ];
+
+    expensesTable.sort.apply(sortFields);
+    return context.sync();
+  }).catch(function(error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
+
+export const filterTable = () => {
+  Excel.run(function(context) {
+    const currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+    const expensesTable = currentWorksheet.tables.getItem("ExpensesTable");
+    const categoryFilter = expensesTable.columns.getItem("Category").filter;
+    categoryFilter.applyValuesFilter(["Education", "Groceries"]);
+
+    return context.sync();
+  }).catch(function(error) {
+    console.log("Error: " + error);
+    if (error instanceof OfficeExtension.Error) {
+      console.log("Debug info: " + JSON.stringify(error.debugInfo));
+    }
+  });
+}
--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -4,9 +4,11 @@
  */
 
 // images references in the manifest
-import "../../assets/icon-16.png";
-import "../../assets/icon-32.png";
-import "../../assets/icon-80.png";
+// import "../../assets/icon-16.png";
+// import "../../assets/icon-32.png";
+// import "../../assets/icon-80.png";
+
+import { createTable, filterTable, sortTable } from '../functions';
 
 /* global console, document, Excel, Office */
 
@@ -25,71 +27,3 @@ Office.onReady(info => {
     document.getElementById("sort-table").onclick = sortTable;
   }
 });
-
-function createTable() {
-  Excel.run(function(context) {
-    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    var expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
-    expensesTable.name = "ExpensesTable";
-
-    expensesTable.getHeaderRowRange().values = [["Date", "Merchant", "Category", "Amount"]];
-
-    expensesTable.rows.add(null /*add at the end*/, [
-      ["1/1/2017", "The Phone Company", "Communications", "120"],
-      ["1/2/2017", "Northwind Electric Cars", "Transportation", "142.33"],
-      ["1/5/2017", "Best For You Organics Company", "Groceries", "27.9"],
-      ["1/10/2017", "Coho Vineyard", "Restaurant", "33"],
-      ["1/11/2017", "Bellows College", "Education", "350.1"],
-      ["1/15/2017", "Trey Research", "Other", "135"],
-      ["1/15/2017", "Best For You Organics Company", "Groceries", "97.88"]
-    ]);
-
-    expensesTable.columns.getItemAt(3).getRange().numberFormat = [["\u20AC#,##0.00"]];
-    expensesTable.getRange().format.autofitColumns();
-    expensesTable.getRange().format.autofitRows();
-
-    return context.sync();
-  }).catch(function(error) {
-    console.log("Error: " + error);
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo));
-    }
-  });
-}
-
-function filterTable() {
-  Excel.run(function(context) {
-    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    var expensesTable = currentWorksheet.tables.getItem("ExpensesTable");
-    var categoryFilter = expensesTable.columns.getItem("Category").filter;
-    categoryFilter.applyValuesFilter(["Education", "Groceries"]);
-
-    return context.sync();
-  }).catch(function(error) {
-    console.log("Error: " + error);
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo));
-    }
-  });
-}
-
-function sortTable() {
-  Excel.run(function(context) {
-    var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
-    var expensesTable = currentWorksheet.tables.getItem("ExpensesTable");
-    var sortFields = [
-      {
-        key: 1, // Merchant column
-        ascending: false
-      }
-    ];
-
-    expensesTable.sort.apply(sortFields);
-    return context.sync();
-  }).catch(function(error) {
-    console.log("Error: " + error);
-    if (error instanceof OfficeExtension.Error) {
-      console.log("Debug info: " + JSON.stringify(error.debugInfo));
-    }
-  });
-}
